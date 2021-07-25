0e896d4b6561194cd146bff24b797ec54f61a4e3
Stepan <digital-souris@ya.ru>
2021-07-25 12:38:51 +0300

    add footer

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,29 @@
 <template>
   <div id="app">
-    <main-header></main-header>
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="page">
+      <div class="page__content">
+        <main-header></main-header>
+        <router-view/>
+      </div>
+      <div class="page__footer">
+        <main-footer></main-footer>
+      </div>
     </div>
-    <router-view/>
-    <main-footer></main-footer>
   </div>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
+  .page {
+    display: flex;
+    flex-direction: column;
+    min-height: 100%;
+    &__content {
+      flex: 1 0 auto;
+    }
+    &__footer {
+      flex: 0 0 auto;
+    }
+  }
 </style>
 <script>
   import MainHeader from "./components/layouts/MainHeader";
